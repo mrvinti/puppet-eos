67ce139cdc1e3f5d5df6bcd9219b3b69772f9838
Ruben Knaus <ruben.knaus@gmx.ch>
2016-08-24 18:16:57 +0200

    delete all previous file

--- lib/eos/version.rb
+++
@@ -1,5 +0,0 @@
-##
-# eos version
-module Eos
-  VERSION = '0.1.0'
-end
--- spec/support/shared_examples_for_providers.rb
+++
@@ -1,9 +0,0 @@
-RSpec.shared_examples 'provider resource methods' do
-	opts = {}
-	
-  opts.each_pair do |method, value|
-    it "#{method} is #{value}" do
-      expect(subject.send(method)).to eq(value)
-    end
-  end
-end
